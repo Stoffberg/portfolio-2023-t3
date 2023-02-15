8a0bcb9cecce6a57dd7f84c02d9b57967d620ee0
Dirk Beukes <dirk_beukes@hotmail.com>
2023-02-15 14:27:35 +0200

    feat: font optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,23 @@ import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
+import { Inter } from "@next/font/google";
+const interFont = Inter({ subsets: ["latin"] });
+
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --inter-font: ${interFont.style.fontFamily};
+          }
+        `}
+      </style>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["var(--inter-font)", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         main: {
