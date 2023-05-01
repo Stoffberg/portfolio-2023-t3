d3f4680498fa7807c2c333da32bf1254ce68d09b
Dirk Beukes <dirk_beukes@hotmail.com>
2023-05-01 23:18:35 +0200

    fix: set cors headers

--- src/pages/api/hidden.ts
+++ src/pages/api/hidden.ts
@@ -1,6 +1,17 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request (CORS)
+  if (req.method === "OPTIONS") {
+    res.status(204).end();
+    return;
+  }
+
   res.status(200).json({ message: "This is a hidden API route" });
 };
 
