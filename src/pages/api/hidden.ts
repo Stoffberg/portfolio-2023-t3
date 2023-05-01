f0c6e74adaba3043b560d6cfcd48d77f898f136a
Dirk Beukes <dirk_beukes@hotmail.com>
2023-05-01 23:12:51 +0200

    fix: hidden route

--- src/pages/api/hidden.ts
+++ src/pages/api/hidden.ts
@@ -1,7 +1,7 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200);
+  res.status(200).json({ message: "This is a hidden API route" });
 };
 
 export default handler;
